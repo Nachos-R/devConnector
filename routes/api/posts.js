cbb8a67b95a588162683a782e909bfdaa773cd4f
Yura Rizhniv <novocaine19@gmail.com>
2018-08-02 01:39:09 +0300

    Add GET and DELETE post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Post = require('./../../models/Post');
+const Profile = require('./../../models/Profile');
 const validatePostInput = require('./../../validation/post');
 
 const router = express.Router();
@@ -11,6 +12,27 @@ const router = express.Router();
 // @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
+    );
+});
 
 // @route   POST api/posts
 // @desc    Create post
@@ -33,6 +55,30 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: 'User not authorized' });
+          }
+
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'Not found post' }));
+    });
   }
 );
 
