f863d1ae08566e51df2df81958331abef79025d5
Yura Rizhniv <novocaine19@gmail.com>
2018-08-16 14:55:28 +0300

    Added Post actions, PostItem and PostFeed component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,16 +3,31 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import PostForm from './PostForm';
+import PostFeed from './PostFeed';
 import Loader from '../common/Loader';
+import { getPosts } from '../../actions/postActions';
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Loader />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="constainer">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -21,4 +36,15 @@ class Posts extends Component {
   }
 }
 
-export default connect()(Posts);
+Posts.propTypes = {
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
